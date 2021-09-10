3efde9ba234c4292a2de9e9eac0b42263e569f2d
Макс <yashanty@gmail.com>
2021-09-10 15:54:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace converttobinaryhex
+{
+    using System;
+
+    public static class Kata
+    {
+        public static string EvensAndOdds(int num)
+        {
+            // throw new NotImplementedException();
+            //if (num % 2 == 0) return Convert.ToString(num,2);
+            //else return Convert.ToString(num,16);
+
+            //return num % 2 == 0 ? Convert.ToString(num, 2) : Convert.ToString(num, 16);
+
+            return Convert.ToString(num, num % 2 == 0 ? 2 : 16);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.EvensAndOdds(2));
+            Console.WriteLine(Kata.EvensAndOdds(111));
+        }
+    }
+}
